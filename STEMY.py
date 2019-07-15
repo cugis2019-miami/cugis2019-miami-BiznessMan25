c1c133e2b6dba5090d45869cc2d81692a1ac5957
BiznessMan25 <52932057+BiznessMan25@users.noreply.github.com>
2019-07-15 15:53:28 -0400

    Add files via upload

---
+++ STEMY.py
@@ -0,0 +1,28 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+5/8 #division
+
+print("hello world")
+print("I am learning to code in python today")
+print("1 2 3 4 5 6 7 8 9")
+print("yah yeet")
+print(53)
+print(5*2)
+print(5/2) 
+print(5-2)
+print(5**2)
+print(8/9*3)
+print("5 divided by 2 is", 5/2)
+#create fucntion to sum any two numbers
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)
+plus(6,7,9)
+print(1/2*6*9)
