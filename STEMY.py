1f012d065434cdc726c07e1e208c7c0725328281
BiznessMan25 <52932057+BiznessMan25@users.noreply.github.com>
2019-07-16 16:01:21 -0400

    Add files via upload

--- STEMY.py
+++ STEMY.py
@@ -6,19 +6,10 @@ This is a temporary script file.
 """
 
 
-
 5/8 #division
 
 print("hello world")
 print("I am learning to code in python today")
-print("1 2 3 4 5 6 7 8 9")
-print("yah yeet")
-print(53)
-print(5*2)
-print(5/2) 
-print(5-2)
-print(5**2)
-print(8/9*3)
 print("5 divided by 2 is", 5/2)
 #create fucntion to sum any two numbers
 def plus(a,b,c):
@@ -26,3 +17,19 @@ def plus(a,b,c):
     print(plus)
 plus(6,7,9)
 print(1/2*6*9)
+
+box=5 + 5 + 5
+print("There are",box, "bars in the box")
+d = "dark"
+m = "milk"
+w = "white"
+print(d,m,w)
+    
+
+
+
+import math
+dir(math)
+def yeet(x):
+    yeet = x**(1/3)
+    print("the cubic root of", x, "is" ,yeet)
